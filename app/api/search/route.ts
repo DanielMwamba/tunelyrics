f4a1bfb2d8dfa3a8359b53a810b68f42ba2e0d38
Daniel Mwamba <danmuamba81@gmail.com>
2025-02-03 11:20:52 +0100

    feat: add he library for HTML entity decoding and update lyrics extraction logic

--- app/api/search/route.ts
+++ app/api/search/route.ts
@@ -11,7 +11,7 @@ export async function GET(request: Request) {
   const apiKey = '4D-pz_Kz7UX_3scJ01mt7oiZ5wQYULf_cNZ5tqIWKyYIHpwnp1l2WWw7KVD9C1Up';
   const res = await fetch(`https://api.genius.com/search?q=${query}&access_token=${apiKey}`);
 
-  console.log(res);
+  // console.log(res);
   
   if (!res.ok) {
     return NextResponse.json({ error: 'Failed to fetch results' }, { status: 500 });
